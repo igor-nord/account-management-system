15e8a88f12236f3668c484ebe86275b258a823c8
igor <igor.nord@mail.com>
2026-07-05 21:59:13 +0300

    feat(backend): move parameter username to the request header

--- backend/src/main/java/com/homework/customer/infrastructure/controller/CustomerController.java
+++ backend/src/main/java/com/homework/customer/infrastructure/controller/CustomerController.java
@@ -1,9 +1,9 @@
 package com.homework.customer.infrastructure.controller;
 
+import com.homework.common.web.CurrentUsername;
 import com.homework.customer.usecase.FindCustomer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +17,7 @@ class CustomerController {
     }
 
     @GetMapping
-    CustomerResponse byUsername(@RequestParam String username) {
-        return CustomerResponse.of(findCustomer.byUsername(username));
+    CustomerResponse current(@CurrentUsername String username) {
+        return new CustomerResponse(findCustomer.byUsername(username).username());
     }
 }
